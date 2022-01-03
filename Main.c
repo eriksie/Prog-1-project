c553ca6a78df55c80f76bc207b1a2bdcb6c3a00a
eriksie <55592935+eriksie@users.noreply.github.com>
2022-01-03 17:55:39 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,10 +1,57 @@
-//Prog-1-project
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+typedef struct
+{
+    unsigned day,year,month;
+} Date;
+
+typedef struct
+{
+    int id,salary,phone_num;
+    char* Fname,*Lname,*address,*email;
+    Date* Birthday;
+} Employee;
+
+Date* constructDate (int day,int month,int year)
+{
+    Date*x=malloc(sizeof(Date));
+    x->day=day;
+    x->month=month;
+    x->year=year;
+    return x;
+}
+
+Employee* ConstructEmployee (int id,int salary,int Phone_num,char*Fname,char*Lname,char* address, char* email,int day, int month, int year)
+{
+    Employee*x=malloc(sizeof(Employee));
+    x->id=id;
+    x->phone_num=Phone_num;
+    x->salary=salary;
+    x->Fname=(char *) malloc(strlen(Fname)+1);
+    strcpy(x->Fname,Fname);
+    x->Lname=(char *) malloc(strlen(Lname)+1);
+    strcpy(x->Lname,Lname);
+    x->address=(char *) malloc(strlen(address)+1);
+    strcpy(x->address,address);
+    x->email=(char *) malloc(strlen(email)+1);
+    strcpy(x->email,email);
+    x->Birthday=constructDate(day,month,year);
+    return x;
+}
+void DestructEmployee(Employee* x)
+{
+    free(x->Fname);
+    free(x->Lname);
+    free(x->Birthday);
+    free(x->address);
+    free(x->email);
+    free(x);
+}
 int main()
 {
     printf("Hello world!\n");
-    printf("testing");
-    printf("test this");
     return 0;
 }
